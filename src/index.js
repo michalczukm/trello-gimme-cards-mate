fd1e6409b7f573bfc33e3fefae5fd67721648bb4
Michal Michalczuk <michalczukm@gmail.com>
2019-10-11 23:36:20 +0200

    display simple modal

--- src/index.js
+++ src/index.js
@@ -4,20 +4,24 @@ var WHITE_ICON =
     'https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4bc6e88b7618%2Ficon-white.svg';
 
 const boardButtonsHandler = trello => {
-    return trello.popup({
-        title: 'Option 1',
-        items: [
-            {
-                text: 'Text inside: Option 1',
+    return trello.lists('id', 'name').then(lists => {
+        return trello.popup({
+            title: 'Pick list to get content from:',
+            items: lists.map(list => ({
+                text: list.name,
                 callback: trello =>
-                    trello.lists('id', 'name').then(lists => console.log('=== lists:', lists)),
-            },
-        ],
+                    trello.modal({
+                        url: './views/format-model/view.html',
+                        title: `Gimme cards mate for "${list.name}" board`,
+                        fullscreen: false,
+                    }),
+            })),
+        });
     });
 };
 
 initializeTrelloPowerUp({
-    'board-buttons': function(t, options) {
+    'board-buttons': () => {
         return [
             {
                 icon: WHITE_ICON,
