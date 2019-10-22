80ed79dc2c0966438d56c6c11aad5f19c6be45d3
Michal Michalczuk <michalczukm@gmail.com>
2019-10-23 01:33:55 +0200

    avoid blinking copy to clipboard-only textarea

--- src/utils/copy-to-clipboard.js
+++ src/utils/copy-to-clipboard.js
@@ -1,11 +1,19 @@
 /**
  * `navigator.clipboard.writeText` cannot be used here,
  *   since we're running our scripts in iframe, and `navigator.clipboard.writeText`
- *   requires access to clipboard via Permissions API
+ *   requires access to clipboard via Permissions API.
+ *   So we have to do this old way.
  */
 export const copyToClipboard = text => {
     const element = document.createElement('textarea');
     element.value = text;
+    element.setAttribute('readonly', '');
+
+    // prevent "blinking" textarea, we cannot made it hidden or 'display:none' because those elements
+    //   are not-selectable
+    element.style.position = 'absolute';
+    element.style.left = '-9999px';
+    element.style.zIndex = '-10000';
 
     document.body.appendChild(element);
 
